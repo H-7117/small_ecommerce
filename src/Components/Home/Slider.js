84fe7da8ba621df4910107b356e1405cde720771
Hamood Abdulrqeb Ahmed Saif <81177256+H-7117@users.noreply.github.com>
2023-07-10 16:22:49 +0300

    add design to Slider Page

--- src/Components/Home/Slider.js
+++ src/Components/Home/Slider.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import slider4 from "../../Images/slider4.png";
+import sliderimg from "../../Images/slider1.png";
+import prod3 from "../../Images/prod3.png";
+import prod4 from "../../Images/prod4.png";
 
 const Slider = () => {
-  return (
-    <div>Slider</div>
-  )
-}
+   const [index,setIndex]  = useState()
+   const handleSelect = (selectedIndex) =>{
+    setIndex(selectedIndex)
+   }
 
-export default Slider
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item className="slider-background" interval={2000}>
+        <div className="d-flex flex-row justify-content-center align-items-center">
+          <img
+            style={{ height: "296px", width: "313.53px" }}
+            className=""
+            src={slider4}
+            alt="first slider"
+          />
+          <div className="">
+            <h3 className="slider-title">هناك خصم كبير</h3>
+            <p className="slider-text">خصم يصل 50% عند شرائك</p>
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item className="slider-background2" interval={2000}>
+        <div className="d-flex flex-row justify-content-center align-items-center">
+          <img
+            style={{ height: "296px", width: "313.53p" }}
+            className=""
+            src={sliderimg}
+            alt="second slider"
+          />
+          <div className="">
+            <h3 className="slider-title">هناك خصم كبير</h3>
+            <p className="slider-text">خصم يصل 50% عند شرائك</p>
+          </div>
+        </div>
+      </Carousel.Item>
+
+      <Carousel.Item className="slider-background3" interval={2000}>
+        <div className="d-flex flex-row justify-content-center align-items-center">
+          <img
+            style={{ height: "296px", width: "313.53p" }}
+            className=""
+            src={prod3}
+            alt="second slider"
+          />
+          <div className="">
+            <h3 className="slider-title">هناك خصم كبير</h3>
+            <p className="slider-text">خصم يصل 50% عند شرائك</p>
+          </div>
+        </div>
+      </Carousel.Item>
+
+      <Carousel.Item className="slider-background4" interval={2000}>
+        <div className="d-flex flex-row justify-content-center align-items-center">
+          <img
+            style={{ height: "296px", width: "313.53p" }}
+            className=""
+            src={prod4}
+            alt="second slider"
+          />
+          <div className="">
+            <h3 className="slider-title">هناك خصم كبير</h3>
+            <p className="slider-text">خصم يصل 50% عند شرائك</p>
+          </div>
+        </div>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export default Slider;
