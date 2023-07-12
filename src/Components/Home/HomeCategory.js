63944734932d1dcf037d53e1acc76c46a89776fd
Hamood Abdulrqeb Ahmed Saif <81177256+H-7117@users.noreply.github.com>
2023-07-12 13:53:32 +0300

    add HomeCategory and style it

---
+++ src/Components/Home/HomeCategory.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+import SubTitle from '../utility/SubTitle'
+
+const HomeCategory = () => {
+  return (
+    <Container>
+        <SubTitle title="التصنيفات" btntitle="المزيد"/>
+    </Container>
+  )
+}
+
+export default HomeCategory
--- src/page/Home/HomePage.js
+++ src/page/Home/HomePage.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import NavBarLogin from '../../Components/utility/NavBarLogin'
 import Slider from '../../Components/Home/Slider'
+import HomeCategory from '../../Components/Home/HomeCategory'
+
+
 const HomePage = () => {
   return (
     
     <div className='font'>
       <NavBarLogin />
       <Slider />
+      <HomeCategory/>
     </div>
     
   )
