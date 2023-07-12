25f2815a2585885f365e639cc7b6f22bea525a52
Hamood Abdulrqeb Ahmed Saif <81177256+H-7117@users.noreply.github.com>
2023-07-12 17:16:13 +0300

    add footer and style it and added to the HomePage

---
+++ src/Components/Brand/CardBrandComponent.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Container ,Row} from 'react-bootstrap'
+import BrandCard from './BrandCard'
+import SubTitle from '../utility/SubTitle'
+import brand1 from '../../Images/brand1.png'
+import brand2 from '../../Images/brand2.png'
+import brand3 from '../../Images/brand3.png'
+
+const CardBrandComponent = ({title,btntitle}) => {
+  return (
+    <Container>
+    <SubTitle title={title} btntitle={btntitle} />
+    <Row className='my-1 d-flex justify-content-between'>
+        <BrandCard img={brand1}/>
+        <BrandCard img={brand2}/>
+        <BrandCard img={brand3}/>
+        <BrandCard img={brand1}/>
+        <BrandCard img={brand2}/>
+        <BrandCard img={brand3}/>
+    </Row>
+</Container>
+  )
+}
+
+export default CardBrandComponent
+
+
--- src/page/Home/HomePage.js
+++ src/page/Home/HomePage.js
@@ -4,6 +4,9 @@ import Slider from '../../Components/Home/Slider'
 import HomeCategory from '../../Components/Home/HomeCategory'
 import CardProductComponent from '../../Components/Product/CardProductComponent'
 import DiscountSection from '../../Components/Home/DiscountSection'
+import CardBrandComponent from '../../Components/Brand/CardBrandComponent'
+import Footer from '../../Components/utility/Footer'
+
 
 const HomePage = () => {
   return (
@@ -15,6 +18,8 @@ const HomePage = () => {
       <CardProductComponent title="الاكثر مبيعا" btntitle="المزيد"/>
       <DiscountSection />
       <CardProductComponent title="احدث الازياء" btntitle="المزيد" />
+      <CardBrandComponent title="المركات العالميه" btntitle="المزيد"/>
+      <Footer />
     </div>
     
   )
