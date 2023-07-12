70f69956d8fe40eb3623c984a5e7712c33ef47e8
Hamood Abdulrqeb Ahmed Saif <81177256+H-7117@users.noreply.github.com>
2023-07-12 14:18:39 +0300

    design Home page 2

--- src/Components/Category/CategoryCard.js
+++ src/Components/Category/CategoryCard.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Col } from "react-bootstrap";
 
-const CategoryCard = () => {
+const CategoryCard = ({ background, img, title }) => {
   return (
-    <div>CategoryCard</div>
-  )
-}
+    <Col
+      xs="6"
+      sm="6"
+      md="4"
+      lg="2"
+      className="my-4 d-flex justify-content-around"
+    >
+      <div className="allCard mb-3">
+        <div
+          className="categoty-card"
+          style={{ backgroundColor: `${background}` }}
+        ></div>{" "}
+        <img alt="productImage" src={img} className="categoty-card-img" />
+        <p className="categoty-card-text my-2">{title}</p>
+      </div>
+    </Col>
+  );
+};
 
-export default CategoryCard
+export default CategoryCard;
--- src/Components/Home/HomeCategory.js
+++ src/Components/Home/HomeCategory.js
@@ -1,11 +1,25 @@
 import React from 'react'
-import {Container} from 'react-bootstrap'
+import {Container ,Row} from 'react-bootstrap'
 import SubTitle from '../utility/SubTitle'
+import CategoryCard from '../Category/CategoryCard'
+import clothe from '../../Images/clothe.png'
+import cat2 from '../../Images/cat2.png'
+import labtop from '../../Images/labtop.png'
+import sale from '../../Images/sale.png'
+import pic from '../../Images/pic.png'
 
 const HomeCategory = () => {
   return (
     <Container>
+        <Row className='my-2 d-flex justify-content-between'>
         <SubTitle title="التصنيفات" btntitle="المزيد"/>
+        <CategoryCard background='#F4DBA4' img={clothe} title="اجهزه" />
+        <CategoryCard background='#FFFF00' img={cat2} title="اجهزه" />
+        <CategoryCard background='#0034FF' img={labtop} title="اجهزه" />
+        <CategoryCard background='#F4DBA4' img={sale} title="اجهزه" />
+        <CategoryCard background='#FF6262' img={pic} title="اجهزه" />
+        <CategoryCard background='#F4DBA4' img={clothe} title="اجهزه" />
+        </Row>
     </Container>
   )
 }
