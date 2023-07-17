f1f52143da1a3eec07002db1d5ae331a62b1350a
Hamood Abdulrqeb Ahmed Saif <81177256+H-7117@users.noreply.github.com>
2023-07-17 17:18:20 +0300

    add SideFilter and added to ShopProductsPage

--- src/page/Products/ShopProductsPage.js
+++ src/page/Products/ShopProductsPage.js
@@ -1,18 +1,29 @@
-import React from 'react'
-import CategoryHeader from '../../Components/Category/CategoryHeader'
-import SearchCount from '../../Components/utility/SearchCount'
-import { Container } from 'react-bootstrap'
+import React from "react";
+import CategoryHeader from "../../Components/Category/CategoryHeader";
+import SearchCount from "../../Components/utility/SearchCount";
+import { Container, Row, Col } from "react-bootstrap";
+import SideFilter from "../../Components/utility/SideFilter";
+import CardProductComponent from "../../Components/Product/CardProductComponent";
+import Pagination from "../../Components/utility/Pagination";
 
 const ShopProductsPage = () => {
   return (
-    <div style={{minHeight:"650px"}}>
-        <CategoryHeader/>
-        <Container>
-
-        <SearchCount title=' الترتيب 400'/>
-        </Container>
+    <div style={{ minHeight: "650px" }}>
+      <CategoryHeader />
+      <Container>
+        <SearchCount title=" الترتيب 400" />
+        <Row className="d-flex flex-row">
+          <Col sm="2" xs="2" md="1" className="d-flex">
+            <SideFilter />
+          </Col>
+          <Col  sm="10" xs="10" md="11">
+            <CardProductComponent title="" btntitle=""/>
+          </Col>
+        </Row>
+        <Pagination/>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default ShopProductsPage
+export default ShopProductsPage;
